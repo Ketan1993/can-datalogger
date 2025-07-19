18c9499bbd142e79f023b64f5f97118a1f8fe13d
Ketan Vadodariya <ketan.vadodariya@tranetechnologies.com>
2025-07-19 13:31:19 +0530

    files updates with base

--- my_package/interfaces/pcan/pcan.py
+++ my_package/interfaces/pcan/pcan.py
@@ -1,13 +1,51 @@
-"""PCAN Hardware class"""
+"""PCAN Hardware class
+   PCAN Class will provide the API to intract with the Hardware of PCAN.
+"""
 
 from typing import Any, Optional
+from ctypes import *
+from ctypes.util import find_library
 import logging
+import platform
 
 #import module
 from my_package import(
     BusABC
 )
+
+#get the platform type
+PLATFORM = platform.system()
+IS_WINDOWS = PLATFORM == "Windows"
+
 logger = logging.getLogger(__name__)
+
+"""PCAN Hardware class. 
+   provide information about the PCAN Hardware like status of the deivce, provide API to read or write to hardware.
+"""
+class PCANHardware():
+    def __init__(self):
+    
+        """Initialize with type of platform"""
+    
+        if IS_WINDOWS:
+            load_libray_func = windll.LoadLibrary 
+            lib_name         = "PCANBasic"      
+
+        """let's find the PCANBasic dll library on system, which is provided by the PCAN hardware maker.
+        """
+        lib_path = find_library(lib_name)
+        
+        if not lib_path:
+            raise OSError(f"'{lib_name}' not found in system")
+        
+        #load the PCANBasic.dll library from the system
+        try:
+            #this approch is equivalent to windll.LoadLibrary("PCANBasic") method
+            self._m_ddlbasic = load_libray_func(lib_path)
+        except OSError:
+            raise OSError(f"PCAN Basic Library could not loaded. '{lib_path}'")
+
+
 
 class PcanBus(BusABC):
     def __init__(self,
@@ -16,7 +54,8 @@ class PcanBus(BusABC):
                  **kwargs: Any):
        
        logger.info("__init__ of PcanBus class initialize..")
-       
+       self.__mHardwareObject = PCANHardware()
+
        self._device_id = device_id
        logger.info(f"channel name: '{channel}'")
 
